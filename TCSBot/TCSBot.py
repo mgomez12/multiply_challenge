36f8aff9d5028469529785a3ffb49cd82d98827c
mgomez12 <mgomez224758@gmail.com>
2022-03-11 01:44:20 -0500

    comments, comments comments

--- TCSBot/TCSBot.py
+++ TCSBot/TCSBot.py
@@ -1,93 +1,307 @@
 from telnetlib import Telnet
 import csv
 
-class TCSBot():
+
+class RobotError:
+    """
+    Special class for robot errors, in order to distinguish robot error output from
+    """
+    def __init__(self, resp, attempted_command):
+        self.resp = tuple(resp)
+        self.cmd = attempted_command
+        
+    def __repr__(self):
+        return "RobotError: Command " + self.cmd + ", Response: " + " ".join(self.resp)
+
+    def get_error_output(self):
+        return self.resp
+    def get_faulty_command(self):
+        return self.cmd
+
+
+
+
+
+class TCSBot:
+    """
+    Main TCSBot class, uses TCS commands to communicate
+
+    """
 
     
     def __init__(self, sim=None, ip='192.168.0.1', default_cartesian=True):
+        """
+        Creates new instance of bot driver and connects to either Telnet or a
+        simulated server (sim must have write(s) and resp() commands). 
+
+            Parameters:
+                sim(optional): a class with write and response commands to simulate
+                               Telnet activity
+                ip(optional): IP address of robot arm
+                default_cartesian(optional): set either cartesian or angle system
+        """
         
         self.sim = sim
         if not sim:
             self.tn = Telnet(ip)
         resp = self.attach()
-        if (resp[0] != 0):
+        if (resp != 0):
+            print(resp)
             raise Exception("Unable to attach")
         self.default_cartesian = default_cartesian
         
         
     def send_cmd(self, s):
+        """
+        Sends a command to the robot and returns the response
+
+            Parameters:
+                s: string to send
+            Returns: list of returned strings from robot if successful, or RobotError instance otherwise
+        """
+        
         if self.sim:
             self.sim.write(s)
         else:
             tn.write(s.encode('ascii') + b"\n")
-        return self.get_resp()
+        resp = self.get_resp()
+        if resp[0] == '0':
+            return resp
+        return RobotError(resp, s)
+    
     def get_resp(self):
+        """
+        Obtains response from robot
+
+            Returns: list of returned strings from robot
+        """
+        
         if self.sim:
-            return [int(i) for i in (self.sim.resp().split(' '))]
+            return self.sim.resp().split(' ')
         else:
-            return [int(i) for i in tn.read_until('\n').decode('ascii').split(' ')]
+            return tn.read_until('\n').decode('ascii').split(' ')
 
     
     def read_from_csv(self, fname, cartesian=None):
+        """
+        Loads positions from csv file. Each row must be formatted as follows:
+            ix, coord1, coord2, coord3, etc
+            ix, coord1, coord2, coord3, etc
+
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
         if (cartesian == None):
             cartesian = self.default_cartesian
         with open(fname, 'r') as f:
             reader = csv.reader(f)
             for row in reader:
-                resp = self.send_loc(row[0], tuple(row[1:]), cartesian)
-                if (resp[0] != 0):
+                resp = self.set_station(row[0], tuple(row[1:]), cartesian)
+                if (resp != 0):
                     return resp
+        return 0
                 
         
     def attach(self):
-        return self.send_cmd("attach 1")
-    def release(self):
-        return self.send_cmd("attach 0")
-    def shutdown(self):
-        self.release() # add error checking
-        return self.send_cmd("exit")
+        """
+        Releases robot
+
+            Returns: 0 if successful, or RobotError instance if not
+        """
         
-    def send_loc(self, ix, position, cartesian=None):
+        resp = self.send_cmd("attach 1")
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+    
+    def release(self):
+        """
+        Releases robot
+
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
+        resp = self.send_cmd("attach 0")
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+    
+    def shutdown(self):
+        """
+        Releases robot and closes communication channel
+
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
+        resp = self.release()
+        if (resp != 0):
+            return resp
+        resp = self.send_cmd("exit")
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+        
+    def set_station(self, ix, position, cartesian=None):
+        """
+        Sets a station to a position that is passed in.
+
+            Parameters:
+                ix: station index to use
+                position: a tuple containing the position coordinates
+                cartesian(optional): boolean stating if working with cartesian, defaults
+                                     to value defined in constructor if not included
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
         cmd = ' '.join([str(i) for i in position])
         if (cartesian == None):
             cartesian = self.default_cartesian
         if (cartesian):
-            return self.send_cmd(f'locXyz {ix} ' + cmd)
+            resp = self.send_cmd(f'locXyz {ix} ' + cmd)
         else:
-            return self.send_cmd(f'locAngles {ix} '  + cmd)
+            resp = self.send_cmd(f'locAngles {ix} '  + cmd)
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
         
     def station_move(self, ix, profile):
-        return self.send_cmd(f'Move {ix} {profile}')
+        """
+        Moves robot to a station
+
+            Parameters:
+                ix: station index to use
+                profile: profile index to use
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        resp = self.send_cmd(f'Move {ix} {profile}')
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+
     
     def get_curr_loc(self, cartesian=None):
+        """
+        Obtains current location
+
+            Parameters:
+                cartesian(optional): boolean stating if working with cartesian, defaults
+                                     to value defined in constructor if not included
+            Returns: current position tuple if successful, or RobotError instance if not
+        """
         if (cartesian == None):
             cartesian = self.default_cartesian
         if cartesian:
-            return self.send_cmd("wherec")
+            resp = self.send_cmd("wherec")
         else:
-            return self.send_cmd("wherej")
+            resp = self.send_cmd("wherej")
+
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return tuple([int(i) for i in resp[1:]])
+
 
     def get_goal_loc(self, cartesian=None):
+        """
+        Obtains goal location
+
+            Parameters:
+                cartesian(optional): boolean stating if working with cartesian, defaults
+                                     to value defined in constructor if not included
+            Returns: goal position tuple if successful, or RobotError instance if not
+        """
+        
         if (cartesian == None):
             cartesian = self.default_cartesian
         if cartesian:
-            return self.send_cmd("DestC")
+            resp = self.send_cmd("DestC")
         else:
-            return self.send_cmd("DestJ")
+            resp = self.send_cmd("DestJ")
+
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return tuple([int(i) for i in resp[1:]])
 
 
     #note: might not be future proof if default values get updated in robot since
     # they would also have to be changed here
     def teach_plate(self, ix, z_clearance=50):
-        return self.send_cmd(f'TeachPlate {ix} {z_clearance}')
-    def pick_plate(self, ix, horiz_comp=0, comp_torque=0):
-        return self.send_cmd(f'PickPlate {ix} {horiz_comp} {comp_torque}')
-    def place_plate(self, ix, horiz_comp=0, comp_torque=0):
-        return self.send_cmd(f'PlacePlate {ix} {horiz_comp} {comp_torque}')
-    def move_plate(self, from_ix, to_ix, horiz_comp=0, comp_torque=0):
+        """
+        Teaches robot plate location
+
+            Parameters:
+                ix: staton index to use
+                z_clearance(optional): Z clearance to use, defaults to robot default of 50
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
+        resp = self.send_cmd(f'TeachPlate {ix} {z_clearance}')
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+
+        
+    def pick_plate(self, ix, horiz_comp=False, comp_torque=0):
+        """
+        Pick up plate from location
+
+            Parameters:
+                ix: staton index to use
+                horiz_comp: True if should enable horizontal compliance
+                comp_torque: sets % of original horizontal holding torque to be retained during compliance
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
+        resp = self.send_cmd(f'PickPlate {ix} {int(horiz_comp)} {comp_torque}')
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+
+        
+    def place_plate(self, ix, horiz_comp=False, comp_torque=0):
+        """
+        Place plate at location
+
+            Parameters:
+                ix: staton index to use
+                horiz_comp: True if should enable horizontal compliance
+                comp_torque: sets % of original horizontal holding torque to be retained during compliance
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
+        resp = self.send_cmd(f'PlacePlate {ix} {int(horiz_comp)} {comp_torque}')
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
+
+        
+    def move_plate(self, from_ix, to_ix, horiz_comp=False, comp_torque=0):
+        """
+        Pick up plate from one location and set it down at another
+
+            Parameters:
+                ix: staton index to use
+                horiz_comp: True if should enable horizontal compliance
+                comp_torque: sets % of original horizontal holding torque to be retained during compliance
+            Returns: 0 if successful, or RobotError instance if not
+        """
+        
         first_resp = self.pick_plate(from_ix, horiz_comp, comp_torque)
-        if first_resp[0] != 0:
+        if first_resp != 0:
             return first_resp
         else:
-            return self.place_plate(to_ix, horiz_comp, comp_torque)
+            resp = self.place_plate(to_ix, horiz_comp, comp_torque)
+        if type(resp) == RobotError:
+            return resp
+        else:
+            return 0
         
--- test.py
+++ test.py
@@ -2,11 +2,18 @@ from TCSBot import TCSBot
 
 
 class Sim:
+    """
+    Dummy response simulator to test input and output handling. This is NOT
+    ACTUALLY FUNCTIONAL, AND CERTAIN VALID COMMANDS RETURN ERRORS in order to
+    test the driver's error handling mechanisms
+
+    """
     def __init__(self):
         self.is_cart = {}
         self.locs = {}
         self.profiles = {1:{'speed':0}}
         self.curr_loc = (0,0,0, 1, 4, 3)
+        self.dest_loc = (2,4,2,4,5,7)
         self.resp_buf = []
         self.attached = False
         pass
@@ -15,7 +22,7 @@ class Sim:
         args = s.split()
         print(s)
         if (args[0] == "attach"):
-            if args[1] == 1: # add invalid value handling?
+            if args[1] == 1: 
                 self.attached = True
                 self.resp_buf = [0]
             else:
@@ -30,24 +37,41 @@ class Sim:
                 self.resp_buf = [0, args[1]]
         elif (args[0] == "wherec"):
             self.resp_buf = [0] + [i for i in self.curr_loc]
+            
+        elif (args[0] == "DestJ"):
+            self.resp_buf = [0] + [i for i in self.dest_loc]
         else:
-            self.resp_buf = [0]
+            self.resp_buf = [-1]
                 
         
     def resp(self):
         return ' '.join([str(i) for i in self.resp_buf])
 
+
+
+
+#---------------------------------------------------------------#
+
+    
 s = Sim()
 b = TCSBot(s)
 
-print(b.send_loc(0,(1,2,3,1,2,3)))
+print(b.set_station(0,(1,2,3,1,2,3)))
+print()
 print(b.get_curr_loc())
-print(b.send_loc(1, (4, 8, 4), cartesian=False))
-print(b.get_goal_loc())
+print()
+print(b.set_station(1, (4, 8, 4), cartesian=False))
+print()
 print(b.get_goal_loc(cartesian=False))
+print()
 print(b.teach_plate(2))
+print()
 print(b.pick_plate(2))
+print()
 print(b.place_plate(2,1,30))
+print()
 print(b.move_plate(2, 5, 1))
+print()
 print(b.read_from_csv('dummy_locs.csv'))
+print()
 print(b.read_from_csv('dummy_locs.csv', cartesian=False))
